6ccefa968ca1b3a801c8e1e4c1c3c172378d1186
abonello <ab@anthonybonello.co.uk>
2018-01-22 21:07:06 +0000

    create a dataframe

--- pandas_test.py
+++ pandas_test.py
@@ -10,3 +10,8 @@ The Pandas module is a high performance, highly efficient, and high level data a
 start = datetime.datetime(2010, 1, 1)
 end = datetime.datetime(2015, 8, 22)
 
+# create a dataframe
+df = data.DataReader("XOM", "yahoo", start, end)
+
+
+
