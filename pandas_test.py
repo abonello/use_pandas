956bea1fc406beb1b4be52391cc1f0db0e4525d3
abonello <ab@anthonybonello.co.uk>
2018-01-22 21:05:58 +0000

    create start and end variables that are datetime objects

--- pandas_test.py
+++ pandas_test.py
@@ -1,7 +1,12 @@
-import datetime
+import datetime                     # used to tell Pandas some dates that we want to pull data between.
 import pandas
-from pandas_datareader import data
+from pandas_datareader import data  # use this to pull data from the internet. 
+# pandas_datareader was previously known as pandas.io.data
 
 '''
 The Pandas module is a high performance, highly efficient, and high level data analysis library.
 '''
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2015, 8, 22)
+
